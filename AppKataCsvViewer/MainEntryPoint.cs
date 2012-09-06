bebe44e22f32deb64c6a21341b0064927deff75f
Claus Polanka <claus.polanka@chello.at>
2012-09-06 20:52:33 +0200

    CsvViewer formats given csv content. User options must be added and displayed.

--- AppKataCsvViewer/MainEntryPoint.cs
+++ AppKataCsvViewer/MainEntryPoint.cs
@@ -1,49 +1,120 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace AppKataCsvViewer
 {
     public class MainEntryPoint
     {
+        private const string HEADER_SEPARATOR_CHARACTER = "-";
         private const int FILE_NAME = 0;
+        private const string SPACE = " ";
+        private const string COLUMN_SEPARATOR = "|";
+        private const char CSV_SEPARATOR = ';';
+        private const string HEADER_COLUMN_SEPARATOR = "+";
+        private const int HEADER_ROW = 0;
+        
+        private static int[] maxColumnLengths;
 
         public static void Main(string[] args)
         {
             string csvFileName = args[FILE_NAME];
-            int defaultPageSize = 3;
 
-            string[] lines = File.ReadAllLines(csvFileName);
+            var csvRows = File.ReadAllLines(csvFileName);
 
-            int lengthColumn1 = 0;
-            int lengthColumn2 = 0;
-            int lengthColumn3 = 0;
+            maxColumnLengths = DetermineMaxColumnLengthsFor(csvRows);
 
-            foreach (string l in lines)
+            Show(ToFormattedRows(csvRows));
+        }
+
+        private static int[] DetermineMaxColumnLengthsFor(string[] cvsRows)
+        {
+            int numberOfColumns = cvsRows[HEADER_ROW].Split(CSV_SEPARATOR).Length;
+            
+            int[] maxColumnLengths = new int[numberOfColumns];
+
+            foreach (string cvsRow in cvsRows)
             {
-                string[] words = l.Split(';');
+                string[] words = cvsRow.Split(CSV_SEPARATOR);
 
-                if (lengthColumn1 < words[0].Length)
-                    lengthColumn1 = words[0].Length;
-
-                if (lengthColumn2 < words[1].Length)
-                    lengthColumn2 = words[1].Length;
-
-                if (lengthColumn3 < words[2].Length)
-                    lengthColumn3 = words[2].Length;
-            }
-
-            for (int index = 0; index < lines.Length; index++)
-            {
-                Console.Out.WriteLine(lines[index].Replace(';', '|'));
-
-                if (index == 0)
+                for (int i = 0; i < words.Length; i++)
                 {
-                    for (int i = 0; i < lines[index].Length; i++)
-                        Console.Out.Write("-");
-
-                    Console.Out.WriteLine("");
+                    if (maxColumnLengths[i] < words[i].Length)
+                    {
+                        maxColumnLengths[i] = words[i].Length;
+                    }
                 }
             }
+
+            return maxColumnLengths;
+        }
+
+        private static List<List<string>> ToFormattedRows(string[] csvRows)
+        {
+            var formattedRows = new List<List<string>>();
+
+            foreach (string csvRow in csvRows)
+                formattedRows.Add(WithAppliedFormat(csvRow));
+
+            return formattedRows;
+        }
+
+        private static List<string> WithAppliedFormat(string csvRow)
+        {
+            List<string> comaSeparatedValues = csvRow.Split(CSV_SEPARATOR).ToList();
+
+            for (int i = 0; i < comaSeparatedValues.Count; i++)
+                comaSeparatedValues[i] = WithAppliedSeparator(comaSeparatedValues[i], i);
+
+            return comaSeparatedValues;
+        }
+
+        private static string WithAppliedSeparator(string word, int columnIndex)
+        {
+            string formattedWord = word;
+
+            for (var i = 0; i < (maxColumnLengths[columnIndex] - word.Length); i++)
+                formattedWord += SPACE;
+
+            return formattedWord + COLUMN_SEPARATOR;
+        }
+
+        private static void Show(List<List<string>> formattedRows)
+        {
+            for (int rowIndex = 0; rowIndex < formattedRows.Count; rowIndex++)
+            {
+                Show(columns: formattedRows[rowIndex]);
+                ShowHeaderSeparator(rowIndex);
+            }
+        }
+
+        private static void Show(List<string> columns)
+        {
+            foreach (var c in columns)
+                Console.Out.Write(c);
+
+            Console.Out.WriteLine();
+        }
+
+        private static void ShowHeaderSeparator(int tableRowIndex)
+        {
+            if (tableRowIndex != 0)
+                return;
+
+            for (int columnIndex = 0; columnIndex < maxColumnLengths.Length; columnIndex++)
+            {
+                ShowHeaderForColumnWith(maxColumnLengths[columnIndex]);
+                Console.Out.Write(HEADER_COLUMN_SEPARATOR);
+            }
+
+            Console.Out.WriteLine();
+        }
+
+        private static void ShowHeaderForColumnWith(int columnLength)
+        {
+            for (int c = 0; c < columnLength; c++)
+                Console.Out.Write(HEADER_SEPARATOR_CHARACTER);
         }
     }
 }
--- AppKataCsvViewerTests/CsvViewerEndToEndTest.cs
+++ AppKataCsvViewerTests/CsvViewerEndToEndTest.cs
@@ -8,6 +8,11 @@ namespace AppKataCsvViewerTests
     [TestFixture]
     public class CsvViewerEndToEndTest
     {
+        private const string NEW_LINE = "\r\n";
+        private const string CSV_FILE_NAME = "persons.csv";
+        private StringWriter generatedCsvOutput;
+        private TextWriter stdout;
+
         [Test]
         public void ForADefaultPageSizeOf3_ViewerShowsOnePageForGivenCsvData()
         {
@@ -19,35 +24,40 @@ namespace AppKataCsvViewerTests
                 "Mary;35;Munich" 
             };
 
-            File.WriteAllLines("persons.csv", csvContent);
+            CreateTemporaryCsvFileWith(csvContent);
 
-            MainEntryPoint.Main(new[] { "persons.csv" });
+            MainEntryPoint.Main(new[] { CSV_FILE_NAME });
 
-            var expectedTable = new[]
-            {
-                "Name |Age|City    |",
-                "-----+---+--------+",
-                "Peter|42 |New York|", 
-                "Paul |57 |London  |", 
-                "Mary |35 |Munich  |" 
-            };
+            var expected = "Name |Age|City    |" + NEW_LINE + 
+                           "-----+---+--------+" + NEW_LINE +
+                           "Peter|42 |New York|" + NEW_LINE +
+                           "Paul |57 |London  |" + NEW_LINE +
+                           "Mary |35 |Munich  |" + NEW_LINE;
 
-            // Assert against redirected console output.
+           // TODO: Add empty row + user options
 
-            File.Delete("persons.csv");
+            Assert.That(generatedCsvOutput.ToString, Is.EqualTo(expected), "formatted csv output");
+        }
+
+        private static void CreateTemporaryCsvFileWith(string[] csvContent)
+        {
+            File.WriteAllLines(CSV_FILE_NAME, csvContent);
         }
 
         [SetUp]
         public virtual void SetUp()
         {
-            // Redirect Console.Out = ??;
+            stdout = Console.Out;
+            generatedCsvOutput = new StringWriter();
+            Console.SetOut(generatedCsvOutput);
         }
 
         [TearDown]
         public virtual void TearDown()
         {
-            Console.Out.Close();
-            Console.SetOut(new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true });
+            Console.SetOut(stdout);
+            generatedCsvOutput.Close();
+            File.Delete(CSV_FILE_NAME);
         }
     }
 }
