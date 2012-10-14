3a97baa8234895d47d4791ffddeb76669b005d20
Claus Polanka <claus.polanka@chello.at>
2012-10-14 13:36:00 +0200

    Improved test data naming.

--- AppKataCsvViewerUnitTests/PageTest.cs
+++ AppKataCsvViewerUnitTests/PageTest.cs
@@ -7,32 +7,38 @@ namespace AppKataCsvViewerUnitTests
     public class PageTest
     {
         [Test]
-        public void MaxColumnLengths_GivenAPageWith2Records_ReturnMaxLengthForEachColumn()
+        public void MaxColumnLengths_GivenAPageWith3Records_ReturnMaxLengthForEachColumn()
         {
             var header = new DataRecord();
-            header.Add("Name");
-            header.Add("Age");
-            header.Add("City");
+            header.Add("col_1_Maximum");
+            header.Add("col_2");
+            header.Add("col_3");
 
-            var dataRecord = new DataRecord();
-            dataRecord.Add("Claus");
-            dataRecord.Add("30");
-            dataRecord.Add("Vienna");
+            var dataRecord_1 = new DataRecord();
+            dataRecord_1.Add("val_1");
+            dataRecord_1.Add("val_2_Maximum");
+            dataRecord_1.Add("val_3");
+            
+            var dataRecord_2 = new DataRecord();
+            dataRecord_2.Add("val_1");
+            dataRecord_2.Add("val_2");
+            dataRecord_2.Add("val_3_Maximum");
 
-            var page = new Page();
-            page.Add(header);
-            page.Add(dataRecord);
+            var sut = new Page();
+            sut.Add(header);
+            sut.Add(dataRecord_1);
+            sut.Add(dataRecord_2);
 
-            int[] expected = { "Claus".Length, "Age".Length, "Vienna".Length };
+            int[] expectedMaxLengths = { "col_1_Maximum".Length, "val_2_Maximum".Length, "val_3_Maximum".Length };
 
-            Assert.AreEqual(expected, page.MaxColumnLengths(), "page max column lengths");
+            Assert.AreEqual(expectedMaxLengths, sut.MaxColumnLengths(), "page max column lengths");
         }
 
         [Test]
         public void Equals_GivenTwoIdenticalDataRecordsForTwoPages_PagesAreEqual()
         {
             var dataRecord = new DataRecord();
-            dataRecord.Add("Word");
+            dataRecord.Add("Field");
 
             var page1 = new Page();
             page1.Add(dataRecord);
@@ -47,7 +53,7 @@ namespace AppKataCsvViewerUnitTests
         public void DataRecords_GivenTwoIdenticalDataRecordsForTwoPages_PageDataRecordsAreEqual()
         {
             var dataRecord = new DataRecord();
-            dataRecord.Add("Word");
+            dataRecord.Add("Field");
 
             var page1 = new Page();
             page1.Add(dataRecord);
@@ -55,7 +61,7 @@ namespace AppKataCsvViewerUnitTests
             var page2 = new Page();
             page2.Add(dataRecord);
 
-            Assert.That(page1.DataRecords, Is.EqualTo(page2.DataRecords), "page data records must be equal");
+            Assert.That(page1.DataRecords, Is.EqualTo(page2.DataRecords), "page's data records must be equal");
         }
     }
 }
