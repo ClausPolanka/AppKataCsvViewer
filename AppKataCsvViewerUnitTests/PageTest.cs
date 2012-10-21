5bd224487e44ed4360df38eeb9522bb76c46b8c4
Claus Polanka <claus.polanka@chello.at>
2012-10-21 16:04:09 +0200

    Added new test for Page to ensure empty fields get replaced with spaces.

--- AppKataCsvViewerUnitTests/PageTest.cs
+++ AppKataCsvViewerUnitTests/PageTest.cs
@@ -6,6 +6,8 @@ namespace AppKataCsvViewerUnitTests
     [TestFixture]
     public class PageTest
     {
+        private const string NL = "\n";
+
         [Test]
         public void Header_GivenDataRecordWithThreeFields_ReturnsCorrectlyFormattedHeader()
         {
@@ -40,7 +42,7 @@ namespace AppKataCsvViewerUnitTests
             sut.Add(headerRecord);
             sut.Add(dataRecord);
 
-            var expectedDataRecords = "\nfield_1      |field_2      |field_3      |\n";
+            var expectedDataRecords = NL + "field_1      |field_2      |field_3      |" + NL;
 
             Assert.That(sut.DataRecords, Is.EqualTo(expectedDataRecords), "page data records");
         }
@@ -58,6 +60,28 @@ namespace AppKataCsvViewerUnitTests
             page2.Add(dataRecord);
 
             Assert.That(page1, Is.EqualTo(page2), "pages must be equal");
+        }
+
+        [Test]
+        public void DataRecords_GivenOneDataRecordContaingEmptyField_TranslateEmptyFieldToSpaces()
+        {
+            var headerRecord = new DataRecord();
+            headerRecord.Add("headerField_1");
+            headerRecord.Add("headerField_2");
+            headerRecord.Add("headerField_3");
+
+            var dataRecord = new DataRecord();
+            dataRecord.Add("field_1");
+            dataRecord.Add(string.Empty);
+            dataRecord.Add("field_3");
+
+            Page sut = CreatePage();
+            sut.Add(headerRecord);
+            sut.Add(dataRecord);
+            
+            string expectedDataRecords = NL + "field_1      |             |field_3      |" + NL;
+
+            Assert.That(sut.DataRecords, Is.EqualTo(expectedDataRecords), "page data records");
         }
 
         private static Page CreatePage()
